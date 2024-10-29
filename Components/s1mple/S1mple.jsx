9b88207d8da1fec1728ac236189b7ef215128363
Java <javohirabidov78@gmail.com>
2024-10-29 15:49:32 +0500

    rasm joyladm

---
+++ Components/s1mple/S1mple.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Main =  () => {
+    return(
+        <div>dhsohdsoihdw</div>
+
+    );
+}
+
+
+export default Main
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-
+import S1mple from  '@/Components/s1mple/S1mple'
 
 const Home = () => {
   return (
     <>
     salom
+    <S1mple />
     </>
   )
 }
